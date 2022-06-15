580092b98f75ed8bfc8731e0f31f8e4fe85b82f6
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-15 15:25:49 +0100

    Create bodmas.js

---
+++ homework/javascript-exercises/bodmas.js
@@ -0,0 +1,9 @@
+// 1. Assign a variable with a name bodmasExample to 3 
+// 2. Add 5 to the variable and divide the result by 6 
+// 3. Console.log the result
+
+let bodmasExample = 3;
+bodmasExample = bodmasExample + 5 / 6;
+console.log(Math.floor(bodmasExample)); //does a whole number
+console.log(Math.ceil(bodmasExample)); //rounds to the nearest number up
+console.log(Math.random(bodmasExample)); // returns a floating-point, pseudo-random number in the range 0 to less than 1
