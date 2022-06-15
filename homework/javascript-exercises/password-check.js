05fb5cdc1357bb213e2b973ed601d230cd393586
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-15 17:34:04 +0100

    Create password-check.js

---
+++ homework/javascript-exercises/password-check.js
@@ -0,0 +1,12 @@
+// 1. Create a variable, password
+// 2. Write an if statement which console logs true if the password is over 7 character or false otherwise.
+
+let password = "fiverr";
+
+if(password.length > 7){
+console.log(true);
+} else {
+console.log(false);
+}
+
+//outcome is false because the length is less than 7.
