78c0d65f1f030876e2659e5d8338129358a197dc
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-15 17:37:24 +0100

    Create height-test.js

---
+++ homework/javascript-exercises/height-test.js
@@ -0,0 +1,13 @@
+// 1. To go on the Super Dooper Looper roller coaster you have to be equal or above 150cm
+// 2. Use an if/else statement to log "You can go on the ride" if the variable height is equal or above 150 or "Sorry you are not tall enough" height is below 150
+// 3. Modify your if statement so that if height is not a number the message "Please enter a number" is logged to the console
+
+let height = 150;
+
+if(height === 150){
+console.log("You can go on the ride");
+} else if (height > 150){
+console.log("You can go on the ride");
+} else {
+console.log("Sorry you are not tall enough");
+}
