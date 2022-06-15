4c89ea48d6680b367eecd961edb82c0dc5dc122f
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-15 15:36:36 +0100

    Create uppercase-and-lowercase.js

---
+++ homework/javascript-exercises/uppercase-and-lowercase.js
@@ -0,0 +1,7 @@
+// 1. Define a variable favouriteFood and give it a value of your name
+// 2. Use a method to change favouriteFood to all upper case
+// 3. Console log the result
+
+let favouriteFood = "Chantele";
+console.log(favouriteFood.uppercase);
+console.log(favouriteFood.lowercase);
