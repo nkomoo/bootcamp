0ffac08d3ea78cb4c8df50a4b3b84404456d9479
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-15 15:21:04 +0100

    Create so-many-rabbits.js

---
+++ homework/javascript-exercises/so-many-rabbits.js
@@ -0,0 +1,7 @@
+//1. Define a variable numberOfRabbits as 2
+//2. Set the variable to 3 * the previous number
+//3. Console log the result
+
+let numberOfRabbits = 2 ;
+numberOfRabbits = numberofRabbits * 3
+console.log(numberofRabbits);
