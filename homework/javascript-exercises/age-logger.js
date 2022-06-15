3200b777a58ddf39f1f18048c907fb95e42da732
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-15 15:20:36 +0100

    Create age-logger.js

---
+++ homework/javascript-exercises/age-logger.js
@@ -0,0 +1,7 @@
+//1. Add an external JavaScript file to your HTML page
+//2. Use the let keyword to declare a variable of age
+//3. Set the variable age to your age
+//4.Console log the variable
+let keyword = age;
+age = 23; 
+console.log(age); 
