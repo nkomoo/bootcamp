450affd3a3bc3ba2d8176ad10b79c67514fbeaf8
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-15 17:28:40 +0100

    Create concatenation.js

---
+++ homework/javascript-exercises/concatenation.js
@@ -0,0 +1,14 @@
+// 1. Set your name as a variable
+// 2. Set your age as a variable
+// 3. Define a new variable, message using concatenation as “Hello my name is myName and I am myAge years old” to the console using concatenation.
+// 4. Try changing the variables and console logging
+// 5. Append the string "What is your name?" To the message variable
+
+let name = "Chantele";
+let age = "23";
+message = `Hello my name is ${name} and l am ${age} years old`;
+// OR message = "Hello my name is " + name + " and l am " + age + " years old";
+console.log(message);
+
+
+
