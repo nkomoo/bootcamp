38db3c6521b4801eb873ea3a8f88f92acb1f3b5c
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-17 00:08:22 +0100

    Create lesser-than.js

---
+++ homework/javascript-exercises/lesser-than.js
@@ -0,0 +1,13 @@
+/* Write a function “min(a, b)” which returns the lesser of the two numbers “a” and “b”.
+
+For instance:
+min(1, 2) == 1
+min(10, 100) == 10
+min(1, 1) == 1 */
+
+function min(a,b) {
+return Math.min(a,b);
+}
+
+console.log(min(1, 7));
+console.log(min(10, 100));
