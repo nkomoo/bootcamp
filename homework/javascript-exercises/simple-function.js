0ede5741a72607045bbcdf10b6606f05fc5c1c80
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-16 22:59:37 +0100

    Create simple-function.js

---
+++ homework/javascript-exercises/simple-function.js
@@ -0,0 +1,7 @@
+// 1. Write a function with no parameters that returns the string “Hello World”
+// 2.Try console logging the function
+
+function greet() {
+rerturn "Hello World";
+}
+console.log(greet());
