05b8e3e19063cfe246abe299dc350ac73c5c1c4a
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-17 00:10:19 +0100

    Create to-the-power-of.js

---
+++ homework/javascript-exercises/to-the-power-of.js
@@ -0,0 +1,11 @@
+/* Write a function “pow(x, n)” that returns “x” in power “n”. In other words, it will multiply “x” by itself “n” times (xⁿ) and returns the result.
+pow(2, 2) = 2 * 2 = 4
+pow(2, 3) = 2 * 2 * 2 = 8
+pow(1, 100) = 1 * 1 * …x97 * 1 = 1 */
+
+function pow(x,n){
+return Math.pow(x,n);
+}
+
+pow(2, 2); // 4
+pow(2, 3); // 8
