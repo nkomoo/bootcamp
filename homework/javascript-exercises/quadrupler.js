3b86d91fe82fe94f6d2e94145e97065089e30401
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-16 23:02:11 +0100

    Create quadrupler.js

---
+++ homework/javascript-exercises/quadrupler.js
@@ -0,0 +1,11 @@
+// 1. Create a function called “quadrupler”
+// 2. The function should take in one parameter
+// 3. The function should return 4 times the parameter
+// 4. Try console logging the function with different numbers as parameters
+
+function quadrupler(fourTimes) {
+return fourTimes * 4;
+}
+console.log(quadrupler(3));
+
+// returns 3 * 4 = 12
