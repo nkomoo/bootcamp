3467191344430d13248c7154d47a39a2ab00968c
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-20 18:59:52 +0100

    Create array-exercise-sort.js

---
+++ homework/javascript-exercises/array-exercise-sort.js
@@ -0,0 +1,9 @@
+// 1. Create a function that takes in an array and sorts it into alphabetical order.
+// 2. Try sorting your breakfast array
+
+function newArr(breakfast){
+return breakfast;
+
+}
+let breakfast = ["A", "D", "C", "B"];
+console.log(breakfast.sort());
