030480511dd35e0b3b40073eea28fe04f65632ad
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-20 17:34:05 +0100

    Update dog-years-unfinished.js
    Not completed.

--- homework/javascript-exercises/dog-years-unfinished.js
+++ homework/javascript-exercises/dog-years-unfinished.js
@@ -10,19 +10,4 @@ Instructions
 1. Use your age and name variables
 2. Calculate your age in dog years using JavaScript maths operators
 3. Console log your age in dog years using template literals */
-
-// this is my age and name
-let myAge = 23;
-let myName = "Chantele";
-
-const earlyYears = myAge - 2;
-
-//2nd year of dog is equal to 9 human years
-if (myAge <= 2){
-	console.log(myAge * 9);
-}
-
-//after that each dog year is 5 human years
-
-else if(myAge > 2){
-console.log(myAge + earlyYears * 5);
+// not completed
