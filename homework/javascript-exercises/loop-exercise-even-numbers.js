7f8ad117e3b4368ee46aa3d880196f7b3f8fc72a
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-20 21:14:55 +0100

    Create loop-exercise-even-numbers.js

---
+++ homework/javascript-exercises/loop-exercise-even-numbers.js
@@ -0,0 +1,16 @@
+
+/* Rewrite the code below, changing the “for” loop to a “while” loop without altering the behaviour of the loop.
+
+for ( let x = 0; x < 3; x++ ) {
+
+alert( `number ${x}!` );
+
+}
+*/
+
+let x = 0;
+while (x < 3){
+alert( `number ${x}!` );
+x++
+}
+
