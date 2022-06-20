f31bcead6c48821fa73a8c34292e74b794268089
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-20 21:16:47 +0100

    Create loop-exercise-tasks.js

---
+++ homework/javascript-exercises/loop-exercise-tasks.js
@@ -0,0 +1,8 @@
+// 1. Create an array, called tasks, of 3 or more household tasks you need to do.
+// 2. Use a for loop and template literals to iterate through the array printing `Task ${I+1}: ${tasks[i]}` for each element on the array.
+
+for(let i = 0; i < tasks.length; i++) {
+let tasks = ["Vaccum", "Sweep", "Wash"];
+console.log(`Task ${i+1}: ${tasks[i]}`);
+
+}
