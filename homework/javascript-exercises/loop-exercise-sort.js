5ace2f7f8e8284a70d2ca70f53bff1f00e3da5f2
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-20 21:17:07 +0100

    Create loop-exercise-sort.js

---
+++ homework/javascript-exercises/loop-exercise-sort.js
@@ -0,0 +1,12 @@
+// 1. Use a for loop to log numbers 1-10
+// 2. Modify the for loop so that it counts down from 10 - 1
+
+//count normally 1, 2, 3, 4, 5...
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// count backwards 5, 4, 3, 2, 1...
+for (i = 10; i > 0; i--){
+  console.log(i);
+}
