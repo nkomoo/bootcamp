9699432c4eda5935db27ebfbb6375aaae8c00ae6
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-20 18:59:16 +0100

    Create array-exercise-last-value.js

---
+++ homework/javascript-exercises/array-exercise-last-value.js
@@ -0,0 +1,8 @@
+// 1. Create a function that takes an array as a paramter
+// 2. return the last item of the array
+
+function myArray(number){
+return number[number.length -1];
+}
+let number = [1, 2, 5, 6, 7];
+console.log(myArray(number)); // result is 7
