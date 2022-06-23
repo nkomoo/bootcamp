4e8b3536f2cc47458bca4afc24896c2c7c638767
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-23 21:01:06 +0100

    Create for-each-favouriteFoods.js

---
+++ homework/javascript-exercises/for-each-favouriteFoods.js
@@ -0,0 +1,11 @@
+// 1. Create an array of your favourite foods
+// 2. Use forEach to iterate over the array console logging each item on the list.
+
+const favouriteFoods = ["Lasagna", "Sushi", "Prawn Katsu"];
+
+function faveFoodsArr(arr) {
+arr.forEach((lists) =>{
+console.log(lists);
+})
+}
+console.log(faveFoodsArr(favouriteFoods));
