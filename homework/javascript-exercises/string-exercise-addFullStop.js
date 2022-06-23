512114cad773b72ca57f84be396f874416890d52
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-23 20:58:27 +0100

    Create string-exercise-addFullStop.js

---
+++ homework/javascript-exercises/string-exercise-addFullStop.js
@@ -0,0 +1,9 @@
+// 1. Create a function called addFullStop
+// 2. It should take in a string and the output should always finish with a full stop. Some of the input strings will already have a full stop and others will not.
+
+function addFullStop(string){
+return string;
+}
+
+let string = "Hello";
+console.log(string + ".");
