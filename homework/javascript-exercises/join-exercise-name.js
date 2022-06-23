d56d6ea7f038055df970165fee429cd334a0b2f1
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-23 21:33:56 +0100

    Create join-exercise-name.js

---
+++ homework/javascript-exercises/join-exercise-name.js
@@ -0,0 +1,5 @@
+// 1. Create an array , students, with a list of 3 or more student names
+// 2. Use join to convert the student array to a string with a space in between each name.
+
+let students = ["Ela", "Esther", "Chantele"];
+console.log(students.join(" "));
