7cf1a92d8bae6da31323890e3833c0fba8792cad
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-26 23:58:30 +0100

    Added extra methods
    Added extra methods for solving the exercise.

--- homework/javascript-exercises/object-exercise-spacecraft.js
+++ homework/javascript-exercises/object-exercise-spacecraft.js
@@ -14,3 +14,25 @@ console.log(spacecraft["range"]);
 for (const i in spacecraft) {
 console.log( i + " " + spacecraft[i]);
 }
+
+//OR using key-value 
+const spacecraft = {
+size: 10,
+range: 200,
+model: 'q10',
+capacity: 200,
+};
+for (const [key, value] of Object.entries(spacecraft)) {
+console.log(`${key} ${value}`);
+}
+
+//or using array extras
+const spacecraft = {
+size: 10,
+range: 200,
+model: 'q10',
+capacity: 200,
+};
+Object.entries(spacecraft).forEach(([key, value]) => {
+console.log(`${key} ${value}`);
+})
