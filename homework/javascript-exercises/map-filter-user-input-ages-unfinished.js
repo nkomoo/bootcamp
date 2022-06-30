5db2e24dcc323ccfab831a9c5709c9cbfe9d08c6
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-06-30 22:03:30 +0100

    Create map-filter-user-input-ages-unfinished.js

---
+++ homework/javascript-exercises/map-filter-user-input-ages-unfinished.js
@@ -0,0 +1,3 @@
+// 1. InputArray is a list of students ages but they are some mistakes.
+// ["18", " 27", 19, 21, "22 ", NaN, Undefined]
+// 2. Create a function which returns a new array of student ages where all the values have been converted to numbers and any missing values have been removed.
